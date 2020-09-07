30139099ac189b51a937d0ebbeadbbbfe842cdd4
KentaKatoh <kenta@diverta.co.jp>
2020-09-08 00:39:26 +0900

    info

---
+++ src/pages/info/_id/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <h2>{{ item.subject }}</h2>
+    <p>
+      {{ item.ymd }}
+    </p>
+    <p v-html="item.contents" />
+  </div>
+</template>
+
+<script>
+export default {
+  auth: false,
+  data: () => ({
+    item: [],
+  }),
+  mounted() {
+    let self = this
+    let url = "/rcms-api/1/info/" + this.$route.params.id
+    this.$auth.ctx.$axios.get(url).then(function (response) {
+      self.item = response.data.details
+    })
+  },
+}
+</script>
